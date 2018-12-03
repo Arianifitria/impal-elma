99c53cffb279bc56ff04074b48273dbe4c206220
Raden Muhamad Irvan R <ipengramadhan@gmail.com>
2018-12-03 09:43:20 +0700

    bacot

--- application/views/produk.php
+++ application/views/produk.php
@@ -1,65 +1,56 @@
-<!-- 1301160464 -->
-<?php $this->load->view('nav_login'); ?>	
+<html>
 <link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>css/produk.css">
-
-
+<head></head>
 <body>
-	<main>
-		<div class="container-field" style="margin-top: 7%; text-align: center;">
-	       	<img src="<?php echo base_url();?>gambar/e-learn.png" width="350" height="350" alt="">
-	       	<h1 style="color: white;">ELMA</h1>
-		</div>
-		<ul class="griddd">
-			<li class="kotak2">
-				<h4 style="text-align: center;">Presensi</h4>
-        		<table style="margin-top: 2%;">
-        			<tr>
+        <main>
+                <div class="container-field" style="margin-top: 7%; text-align: center;">
+                <img src="<?php echo base_url();?>gambar/e-learn.png" width="350" height="350" alt="">
+                <h1 style="color: white;">ELMA</h1>
+                </div>
+                <ul class="griddd">
+                        <li class="kotak2">
+                                <h4 style="text-align: center;">Presensi</h4>
+		
+			<table style="margin-top: 2%;">
+        			<tr style="color: black">
         				<th>No</th>
         				<th>Mata Kuliah</th>
         				<th>Persentase</th>
         			</tr>
-        			<tr>
-        				<th>1.</th>
-        				<th>Pemograman Web</th>
+        			<?php foreach ($matakuliah -> result() as  $value) { ?>
+                                <tr>
+                                
+        				<th><?php echo $value->id_matkul ?></th>
+        				<th><?php echo $value->nama_matkul ?></th>
         				<th>95%</th>
-        			</tr>
-        			<tr>
-        				<th>2.</th>
-        				<th>Implementasi Perancangan Perangkat Lunak</th>
-        				<th>100%</th>
-        			</tr>
-        			<tr>
-        				<th>3.</th>
-        				<th>Jaringan Komputer</th>
-        				<th>90%</th>
-        			</tr>
-        		</table>				
+        			<?php } ?>
+                                </tr>
+
+        		</table>
+                        </li>
+                        <li class="kotak2" style="margin-left: 2%;">
+                        <h4 style="text-align: center;">Nilai</h4>
+                        <table style="margin-top: 2%;">
+                                <tr>
+                                        <th>No</th>
+                                        <th>Mata Kuliah</th>
+                                        <th>Nilai</th>
+                                </tr>
+                                <?php foreach ($matakuliah -> result() as  $value) { ?>
+                                <tr>
+                                
+                                        <th><?php echo $value->id_matkul ?></th>
+                                        <th><?php echo $value->nama_matkul ?></th>
+                                        <th>75</th>
+                                <?php } ?>
+                                </tr>
+
+                        </table>
 			</li>
-			<li class="kotak2" style="margin-left: 2%;">
-				<h4 style="text-align: center;">Nilai</h4>
-        		<table style="margin-top: 2%;">
-        			<tr>
-        				<th>No</th>
-        				<th>Mata Kuliah</th>
-        				<th>Nilai Akhir</th>
-        			</tr>
-        			<tr>
-        				<th>1.</th>
-        				<th>Pemograman Web</th>
-        				<th>77,8</th>
-        			</tr>
-        			<tr>
-        				<th>2.</th>
-        				<th>Implementasi Perancangan Perangkat Lunak</th>
-        				<th>81,5</th>
-        			</tr>
-        			<tr>
-        				<th>3.</th>
-        				<th>Jaringan Komputer</th>
-        				<th>87.2</th>
-        			</tr>
-        		</table>				
-			</li>
-		</ul>
-	</main>
+                </ul>	
+				
+        </main>
 </body>
+</html>
+		
+				
